0135232def599d736c462724170823033f7fe114
Arris Ian Peralta <peraltaarris07@gmail.com>
2024-07-16 13:16:48 +0800

    removed target=_blank from service links

--- app/components/layout/SignInPage.tsx
+++ app/components/layout/SignInPage.tsx
@@ -7,6 +7,7 @@ import { Raleway, Roboto } from 'next/font/google';
 import { createTheme, ThemeProvider } from "@mui/material/styles"; 
 import ButtonComponent from "../ui/ButtonComponent";
 import Link from "next/link";
+import Footer from "./Footer";
 
 const raleway = Raleway({ 
   subsets: ['latin'], 
@@ -32,98 +33,101 @@ export let theme2 = createTheme({
 
 const SignInPage = () => {
   return (
-    <ContainerComponent
-      className={`h-full w-full relative text-slate-700`}
-      classes={{}}
-      fixed={false}
-      disableGutters={true}
-    >
-      <ContentWrapper 
-        className={`mt-20 flex items-center justify-center gap-2`}
+    <>
+      <ContainerComponent
+        className={`h-screen w-full relative text-slate-700`}
+        classes={{}}
+        fixed={false}
+        disableGutters={true}
       >
-        <Link href='/'>
-          <ImageComponent 
-            src={`/assets/filipizen.svg`} 
-            alt={`Filipizen Logo`} 
-            width={250} 
-            height={250} 
-            className={``} 
-            priority
-          />
-        </Link>
-      </ContentWrapper>
-      <ThemeProvider theme={theme}>
         <ContentWrapper 
-          className={`pt-6 flex items-center justify-center`}
-          isSpan={false}
+          className={`mt-20 flex items-center justify-center gap-2`}
         >
-          <FormControl className="w-1/3">
+          <Link href='/'>
             <ImageComponent 
-              src={"/assets/qr-code.png"}
-              alt={"QR Code"}
-              height={300}
-              width={300}
-              className="mx-auto rounded-lg"
+              src={`/assets/filipizen.svg`} 
+              alt={`Filipizen Logo`} 
+              width={250} 
+              height={250} 
+              className={``} 
               priority
             />
-            <Typography 
-              variant='body1' 
-              className="mt-6 text-sm font-medium leading-none flex flex-col gap-2 items-center justify-center"
-              align='center'
-            >
-              Use the Filipizen App to log in via QR code
-              <ContentWrapper className="flex gap-4">
-                <ButtonComponent
-                  variant="text"
-                  className=""
-                  href="https://play.google.com/store/apps"
-                  target="_blank"
-                >
-                  <ImageComponent 
-                    src={"/assets/googleplay.png"} 
-                    alt={"Google Play"} 
-                    width={120}
-                    height={100}
-                  />
-                </ButtonComponent>
-                
-                <ButtonComponent
-                  variant="text"
-                  className=""
-                  href="https://www.apple.com/ph/app-store/"
-                  target="_blank"
-                >
-                  <ImageComponent 
-                    src={"/assets/appstore.png"} 
-                    alt={"App Store"} 
-                    width={120}
-                    height={100}
-                  />
-                </ButtonComponent>
-                
-              </ContentWrapper>
-            </Typography>
-            <ContentWrapper className="mt-3 mx-auto text-center items-center justify-center flex gap-2">
-              <ButtonComponent
-                variant="text"
-                href="/"
-                className="hover:bg-transparent hover:underline text-slate-700"
-              >
-                Terms of Service
-              </ButtonComponent>
-              |
-              <ButtonComponent
-                variant="text"
-                href="/"
-                className="hover:bg-transparent hover:underline text-slate-700"
-              >
-                Privacy Policy
-              </ButtonComponent>
-            </ContentWrapper>
-          </FormControl>        
+          </Link>
         </ContentWrapper>
-      </ThemeProvider>
-    </ContainerComponent>
+        <ThemeProvider theme={theme}>
+          <ContentWrapper 
+            className={`pt-6 flex items-center justify-center`}
+            isSpan={false}
+          >
+            <FormControl className="w-1/3">
+              <ImageComponent 
+                src={"/assets/qr-code.png"}
+                alt={"QR Code"}
+                height={300}
+                width={300}
+                className="mx-auto rounded-lg"
+                priority
+              />
+              <Typography 
+                variant='body1' 
+                className="mt-6 text-sm font-medium leading-none flex flex-col gap-2 items-center justify-center"
+                align='center'
+              >
+                Use the Filipizen App to log in via QR code
+                <ContentWrapper className="flex gap-4">
+                  <ButtonComponent
+                    variant="text"
+                    className=""
+                    href="https://play.google.com/store/apps"
+                    target="_blank"
+                  >
+                    <ImageComponent 
+                      src={"/assets/googleplay.png"} 
+                      alt={"Google Play"} 
+                      width={120}
+                      height={100}
+                    />
+                  </ButtonComponent>
+                  
+                  <ButtonComponent
+                    variant="text"
+                    className=""
+                    href="https://www.apple.com/ph/app-store/"
+                    target="_blank"
+                  >
+                    <ImageComponent 
+                      src={"/assets/appstore.png"} 
+                      alt={"App Store"} 
+                      width={120}
+                      height={100}
+                    />
+                  </ButtonComponent>
+                  
+                </ContentWrapper>
+              </Typography>
+              <ContentWrapper className="mt-3 mx-auto text-center items-center justify-center flex gap-2">
+                <ButtonComponent
+                  variant="text"
+                  href="/"
+                  className="hover:bg-transparent hover:underline text-slate-700"
+                >
+                  Terms of Service
+                </ButtonComponent>
+                |
+                <ButtonComponent
+                  variant="text"
+                  href="/"
+                  className="hover:bg-transparent hover:underline text-slate-700"
+                >
+                  Privacy Policy
+                </ButtonComponent>
+              </ContentWrapper>
+            </FormControl>        
+          </ContentWrapper>
+        </ThemeProvider>
+      </ContainerComponent>
+      <Footer />
+    </>
   )
 }
 
