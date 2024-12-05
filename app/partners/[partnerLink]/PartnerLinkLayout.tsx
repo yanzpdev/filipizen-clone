03f8d272256b050f746ee22efa641a566c3929fd
meg <megp.rameses@gmail.com>
2024-12-05 09:52:08 +0800

    test 5

--- app/partners/[partnerLink]/PartnerLinkLayout.tsx
+++ app/partners/[partnerLink]/PartnerLinkLayout.tsx
@@ -1,24 +1,23 @@
-'use client';
-import Footer from '@/app/components/layout/Footer';
-import Header from '@/app/components/layout/Header';
-import ContentWrapper from '@/app/components/ui/ContentWrapper';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Roboto } from 'next/font/google';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import React from 'react';
+"use client";
+import Footer from "@/app/components/layout/Footer";
+import Header from "@/app/components/layout/Header";
+import ContentWrapper from "@/app/components/ui/ContentWrapper";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-const roboto = Roboto({ 
-  weight: ["400", '500', '700'], 
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
-  display: 'swap'  
+  display: "swap",
 });
 
 export let fontTheme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
-  }
-})
+  },
+});
 
 interface PartnerProps {
   id: number;
@@ -34,7 +33,7 @@ interface PartnerProps {
     name: string;
     objid: string;
     title: string;
-  } 
+  };
   channelid: string;
   isonline: string;
 }
@@ -51,7 +50,7 @@ interface ServiceListProps {
 }
 
 interface ServiceListProps extends Array<ServiceProps> {}
-const PartnerLinkLayout: React.FC<{ data: PartnerProps, serviceList: ServiceProps[] }> = ({ data, serviceList }) => {
+const PartnerLinkLayout: React.FC<{ data: PartnerProps; serviceList: ServiceProps[] }> = ({ data, serviceList }) => {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -63,74 +62,61 @@ const PartnerLinkLayout: React.FC<{ data: PartnerProps, serviceList: ServiceProp
   if (serviceList.length <= 4) {
     const obj = {
       seqno: 1,
-      title: 'void item',
-      objid: 'void',
-      services: []
-    }
-    newServiceList.push(obj)
+      title: "void item",
+      objid: "void",
+      services: [],
+    };
+    newServiceList.push(obj);
   }
 
   return (
     <>
-      {isClient &&
+      {isClient && (
         <>
           <ContentWrapper className={`min-h-[95.3vh] h-full relative`}>
-            <Header 
-              navbarStyles="w-full px-[50px] pb-[5px] pt-[3px] bg-[#2c3e50] h-[50px] flex justify-between items-center" 
-              src={`/assets/partner/${data.id}.png`}
-              height={40}
-              width={40}
-              title={data.title}
-              extraStyle='text-white'
-              page='partner'
-              data={data}
-              headerSelect='services'
-            />
+            <Header navbarStyles="w-full px-[50px] pb-[5px] pt-[3px] bg-[#2c3e50] h-[50px] flex justify-between items-center" src={`/assets/partner/${data.id}.png`} height={40} width={40} title={data.title} extraStyle="text-white" page="partner" data={data} headerSelect="services" />
             <ThemeProvider theme={fontTheme}>
               <ContentWrapper className="mb-[2rem] min-h-[79.7%]">
-                <ContentWrapper className="mx-[80px] px-[32px] h-full"> 
+                <ContentWrapper className="mx-[80px] px-[32px] h-full">
                   <h1 className="mt-[32px] mb-[16px] text-[28px] font-bold leading-none">Select Transaction</h1>
                   <ContentWrapper className="columns-2 w-fit gap-x-5 h-full min-h-[500px]">
-                  {!serviceList ?
+                    {!serviceList ? (
                       <h2 className={`pt-[20px] pb-[5px] leading-none text-slate-800 text-[19.6px] font-semibold`}>No transactions available yet.</h2>
-                    :
+                    ) : (
                       <>
                         {newServiceList.map((service: any, index: number) => (
-                          <ContentWrapper 
-                            key={index} 
-                            className={`w-full break-inside-avoid ${service.objid === "void" && "invisible"}`} // ${ index % 2 === 0 ? 'row-start-1' : 'row-start-2' } 
+                          <ContentWrapper
+                            key={index}
+                            className={`w-full break-inside-avoid ${service.objid === "void" && "invisible"}`} // ${ index % 2 === 0 ? 'row-start-1' : 'row-start-2' }
                           >
                             <h2 className={`pt-[20px] pb-[5px] leading-none text-[#27ae60] text-[19.6px] font-bold order-[${index}]`}>{service.title}</h2>
-                            {service.services.map((subservice: any, subIndex: number) => 
-                              <ContentWrapper 
-                                key={subIndex}
-                                className='flex flex-col leading-relaxed w-fit text-[15.2px] text-[#3f51b5]'
-                              >
-                                <a 
-                                  // href=""
-                                  href={`${data.group.name}_${data.name}/${service.objid}/${subservice.name}`} 
-                                  // href={`http://192.168.2.163:3001/partners/${data.group.name}_${data.name}/${service.objid === 'rptis' && 'rpt'}/${subservice.name}`} 
+                            {service.services.map((subservice: any, subIndex: number) => (
+                              <ContentWrapper key={subIndex} className="flex flex-col leading-relaxed w-fit text-[15.2px] text-[#3f51b5]">
+                                <Link
+                                  prefetch={false}
+                                  href={`${data.group.name}_${data.name}/${service.objid}/${subservice.name}`}
+                                  // href={`http://192.168.2.163:3001/partners/${data.group.name}_${data.name}/${service.objid === 'rptis' && 'rpt'}/${subservice.name}`}
                                   className="hover:underline"
                                 >
                                   {subservice.title}
-                                </a>
+                                </Link>
                               </ContentWrapper>
-                            )}
+                            ))}
                           </ContentWrapper>
                         ))}
                       </>
-                    }
+                    )}
                   </ContentWrapper>
-                  <ContentWrapper className='h-[15px]'></ContentWrapper>
+                  <ContentWrapper className="h-[15px]"></ContentWrapper>
                 </ContentWrapper>
               </ContentWrapper>
             </ThemeProvider>
           </ContentWrapper>
-          <Footer inPartner={true}/>
+          <Footer inPartner={true} />
         </>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
 export default PartnerLinkLayout;
